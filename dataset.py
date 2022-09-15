49efc4ff135fae875362c80cabb2af92f9163db1
Nguyễn Hữu Tiến <91165722+tien-nh@users.noreply.github.com>
2022-09-15 22:52:21 +0700

    l

--- dataset.py
+++ dataset.py
@@ -21,7 +21,7 @@ class StockDataset(Dataset):
         else : 
             self.scaler = scale_to_test
             print("-------------------------")
-            print([corp.shape for corp in corps])
+            print(([corp_.shape for corp_ in corp] for corp in corps))
             self.data = [self.scaler.transform(corp) for corp in corps]
 
         
